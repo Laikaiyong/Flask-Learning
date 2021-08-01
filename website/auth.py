89ceab02ba28052c243b0dbec2c2b85c6ffd1293
Laikaiyong <vandycklai@gmail.com>
2021-08-01 21:41:31 +0800

    Develop half of the tutorial

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    data = request.form
+    print(data)
+    return render_template("login.html", boolean=True)
 
 
 @auth.route('/logout')
@@ -13,6 +15,24 @@ def logout():
     return "<button>Logout</button>"
 
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First Name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            # add user to database
+            flash('Account created', category='success')
+
     return render_template("signup.html")
